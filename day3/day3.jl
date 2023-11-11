f7f4867e3bebb9982785c128df8867e53d172620
thatchatt <aschatterley@gmail.com>
2023-11-11 17:45:14 +0100

    day3 part 2

--- day3/day3.jl
+++ day3/day3.jl
@@ -1,9 +1,22 @@
-sacks = readlines("test3.txt")
+sacks = readlines("input3.txt")
 
 badsum = 0
+badgesum = 0
+elfgroup = [[], [], []]
 
-for s in sacks
+for (n, s) in enumerate(sacks)
     items = collect(s)
+    global elfgroup[(n%3) + 1] = items
+    if n%3 == 0
+        badge = intersect(elfgroup[1], elfgroup[2], elfgroup[3])[1]
+        if islowercase(badge)
+            priority_badge = badge - 'a' + 1
+        else
+            priority_badge = badge - 'A' + 27
+        end
+        global badgesum += priority_badge
+        println("badge is $badge, priority is $priority_badge")
+    end
     comp1 = items[1:Int(floor(length(items)/2))]
     comp2 = items[Int(floor(length(items)/2))+1:end]
     baditem = intersect(comp1, comp2)[1]
@@ -19,4 +32,5 @@ for s in sacks
 end
 
 println("sum is $badsum")
+println("badgesum is $badgesum")
 
